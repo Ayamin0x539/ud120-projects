3106f24f034dc5a807e3889de413082c381cc283
Aya <ayamin1337@yahoo.com>
2019-11-02 20:43:10 -0400

    modularize c value optimization

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,8 +21,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 # Train on 1% of the data to speed it up
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
@@ -32,27 +32,70 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 from timing import time_function
 
-linear_SVM = svm.SVC(kernel='linear')
-print('Using kernel {}'.format(linear_SVM.kernel))
+def linear_svm_classify(features_train, labels_train, features_test, labels_test):
+    '''
+    Perform linear SVM classification on the data.
+    '''
+    linear_SVM = svm.SVC(kernel='linear')
+    print('Using kernel {}'.format(linear_SVM.kernel))
 
-time_function(lambda: linear_SVM.fit(features_train, labels_train), 'linear_SVM.fit()')
+    time_function(lambda: linear_SVM.fit(features_train, labels_train), 'linear_SVM.fit()')
 
-labels_predicted = time_function(lambda: linear_SVM.predict(features_test), 'linear_SVM.predict()')
+    labels_predicted = time_function(lambda: linear_SVM.predict(features_test), 'linear_SVM.predict()')
 
-accuracy = time_function(lambda: accuracy_score(labels_predicted, labels_test), 'accuracy_score()')
+    accuracy = time_function(lambda: accuracy_score(labels_predicted, labels_test), 'accuracy_score()')
 
-print('Linear accuracy: {}'.format(accuracy))
+    print('Linear accuracy: {}'.format(accuracy))
 
-rbf_SVM = svm.SVC(kernel='rbf')
-print('Using kernel {}'.format(rbf_SVM.kernel))
+def get_best_c_value_rbf(features_train, labels_train, features_test, labels_test, c_values):
+    '''
+    From a given set of c values, determine the one that yields the best accuracy.
+    '''
+    best_c_value = -1.0
+    best_accuracy = 0.0
+    for c in c_values:
+        rbf_SVM = svm.SVC(kernel='rbf', C=c, gamma='auto')
+        print('Using kernel {}'.format(rbf_SVM.kernel))
 
-time_function(lambda: rbf_SVM.fit(features_train, labels_train), 'rbf_SVM.fit()')
+        time_function(lambda: rbf_SVM.fit(features_train, labels_train), 'rbf_SVM.fit()')
 
-labels_predicted = time_function(lambda: rbf_SVM.predict(features_test), 'rbf_SVM.predict()')
+        labels_predicted = time_function(lambda: rbf_SVM.predict(features_test), 'rbf_SVM.predict()')
 
-accuracy = time_function(lambda: accuracy_score(labels_predicted, labels_test), 'accuracy_score()')
+        accuracy = time_function(lambda: accuracy_score(labels_predicted, labels_test), 'accuracy_score()')
 
-print('Linear accuracy: {}'.format(accuracy))
+        if accuracy > best_accuracy:
+            best_c_value = c
+
+        print('RBF accuracy with c={}: {}'.format(c, accuracy))
+
+    return best_c_value
+
+best_c_value = get_best_c_value_rbf(features_train[:len(features_train)/100],
+                                    labels_train[:len(labels_train)/100],
+                                    features_test,
+                                    labels_test,
+                                    [10.0, 100.0, 1000.0, 10000.0])
+
+print('Got best C value: {}'.format(best_c_value))
+
+def rbf_classify_with_c(features_train, labels_train, features_test, labels_test, c):
+    '''
+    Perform SVM classification with rbf kernel, using an optimal c parameter passed in.
+    '''
+    rbf_SVM = svm.SVC(kernel='rbf', C=c, gamma='auto')
+    print('Using kernel {}'.format(rbf_SVM.kernel))
+
+    time_function(lambda: rbf_SVM.fit(features_train, labels_train), 'rbf_SVM.fit()')
+    
+    labels_predicted = time_function(lambda: rbf_SVM.predict(features_test), 'rbf_SVM.predict()')
+    
+    accuracy = time_function(lambda: accuracy_score(labels_predicted, labels_test), 'accuracy_score()')
+
+    print('RBF accuracy with c={}: {}'.format(c, accuracy))
+
+
+rbf_classify_with_c(features_train, labels_train, features_test, labels_test, best_c_value)
+
 
 #########################################################
 
