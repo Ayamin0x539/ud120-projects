fe35e6d2606c173cb18f26e6558653cebe570ac1
Aya <ayamin1337@yahoo.com>
2019-11-02 20:16:18 -0400

    Slice test to 1%

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,6 +20,9 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+# Train on 1% of the data to speed it up
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
